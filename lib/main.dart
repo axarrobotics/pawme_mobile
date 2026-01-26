6cbc7b9c96de1f84f4d4698fd7675d8555b15c86
Ashok Jaiswal <ashok.jaiswal@gmail.com>
2026-01-26 12:02:43 +0800

    * Build app icons and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 import 'constants/app_theme.dart';
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 import 'services/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ChangeNotifierProvider(
